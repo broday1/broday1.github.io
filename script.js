532c2c9e72bae59c032eee414fd008c4eca23b32
broday <yaaasbaaz@gmail.com>
2026-07-06 20:41:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -194,3 +194,29 @@ tsParticles.load("particles-js", {
     }
 
 });
+// ===== Cursor =====
+
+const cursor = document.querySelector(".cursor");
+
+if(cursor){
+
+    document.addEventListener("mousemove",(e)=>{
+
+        cursor.style.left = e.clientX + "px";
+        cursor.style.top = e.clientY + "px";
+
+    });
+
+    document.addEventListener("mousedown",()=>{
+
+        cursor.classList.add("active");
+
+    });
+
+    document.addEventListener("mouseup",()=>{
+
+        cursor.classList.remove("active");
+
+    });
+
+}
