dbd0981787a6fc269e7aea287c4099d0b2f0705b
broday <yaaasbaaz@gmail.com>
2026-07-06 14:17:47 +0100

    Create loading.js

---
+++ loading.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+
+    const loader = document.querySelector(".loader");
+
+    setTimeout(() => {
+
+        loader.classList.add("hidden");
+
+    },2500);
+
+});
